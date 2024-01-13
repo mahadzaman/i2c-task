373928a37a42d95a4cfdd74ff07520d7510e918f
Mahad <mahadzaman88@gmail.com>
2024-01-14 00:26:37 +0500

    finalized

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    $('body').on('click', (e) => {
+      if (
+        !$(e.target).closest('.menu-btn').length &&
+        !$(e.target).closest('.menu-open').length
+      ) {
+        this.menuOpen = false;
+        this.body.style.overflow = '';
+      }
+    });
+  }
+  menuOpen: boolean = false;
+  body: any = document.querySelector('body');
+
+  openMenu() {
+    this.menuOpen = true;
+    this.body.style.overflow = 'hidden';
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    this.body.style.overflow = '';
+  }
 }
--- src/app/components/partners/partners.component.ts
+++ src/app/components/partners/partners.component.ts
@@ -6,23 +6,52 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
   standalone: true,
   imports: [SlickCarouselModule],
   templateUrl: './partners.component.html',
-  styleUrl: './partners.component.scss'
+  styleUrl: './partners.component.scss',
 })
 export class PartnersComponent {
   slides = [
-    {img: "assets/images/microsoft-logo.svg", width: 263},
-    {img: "assets/images/twitter-logo.svg", width: 89},
-    {img: "assets/images/coke-logo.svg", width: 301},
-    {img: "assets/images/microsoft-logo.svg", width: 263},
-    {img: "assets/images/twitter-logo.svg", width: 89},
-    {img: "assets/images/coke-logo.svg", width: 301},
-    {img: "assets/images/microsoft-logo.svg", width: 263},
-    {img: "assets/images/twitter-logo.svg", width: 89},
-    {img: "assets/images/coke-logo.svg", width: 301},
-    {img: "assets/images/microsoft-logo.svg", width: 263},
-    {img: "assets/images/twitter-logo.svg", width: 89},
-    {img: "assets/images/coke-logo.svg", width: 301},
+    { img: 'assets/images/twitter-logo.svg', width: 89 },
+    { img: 'assets/images/coke-logo.svg', width: 301 },
+    { img: 'assets/images/microsoft-logo.svg', width: 263 },
+    { img: 'assets/images/twitter-logo.svg', width: 89 },
+    { img: 'assets/images/coke-logo.svg', width: 301 },
+    { img: 'assets/images/microsoft-logo.svg', width: 263 },
+    { img: 'assets/images/twitter-logo.svg', width: 89 },
+    { img: 'assets/images/coke-logo.svg', width: 301 },
+    { img: 'assets/images/microsoft-logo.svg', width: 263 },
+    { img: 'assets/images/twitter-logo.svg', width: 89 },
+    { img: 'assets/images/coke-logo.svg', width: 301 },
+    { img: 'assets/images/microsoft-logo.svg', width: 263 },
   ];
 
-  slideConfig = {"slidesToShow": 3, "slidesToScroll": 1, variableWidth: true, infinite: true,};
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    // autoplay: true,
+    mobileFirst: true,
+    infinite: true,
+    centerMode: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        },
+      },
+    ],
+  };
+
+  breakpoint(e: any) {
+    console.log(e);
+  }
 }
